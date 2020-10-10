c867564ea0684208d148c5f99264993df2365211
Leland Conn <lelandconn@hotmail.com>
2020-10-10 13:54:50 -0400

    Update Country.h

--- Country.h
+++ Country.h
@@ -1,6 +1,5 @@
 /*
   Leland Conn
-  CS 3307 Individual Assignment
   File that provides an abstract outline of the Country class
   Country class will be used to store records of the lines
   in the csv file for each country
